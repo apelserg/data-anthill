1b70dd516378a1a6fa3e2df51d0ddc6d8602ad57
apelserg <apelserg@mail.ru>
2015-08-14 17:06:07 +0300

    new

---
+++ anthill-makedata.cs
@@ -0,0 +1,220 @@
+﻿// ============================
+// Подготовка тестовых данных (для публикации "Учёт в муравейнике")
+// Для получения рабочей программы в Visual Studio надо создать консольный проект, заменить файл program.cs на anthill-makedata.cs и собрать проект
+// ============================
+// Разработчик: apelserg ; https://github.com/apelserg/
+// Лицензия: WTFPL
+// ============================
+
+using System;
+using System.Text;
+using System.IO;
+
+namespace anthill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileAntList = "anthill-ant-list.csv"; // список муравьёв
+            string fileAntType = "anthill-ant-type.csv"; // типы муравьёв
+            string fileCellList = "anthill-cell-list.csv"; // список домов
+            string fileHillType = "anthill-hill-type.csv"; // части (районы) муравейника
+            string fileAntToCell = "anthill-ant-to-cell.csv"; // расселение муравёв по домам
+            string fileLog = "anthill-makedata.log"; // журнал
+
+            int cntAnts = 1000; // 1000 муравьёв - по дефолту
+            int cntBuff = 1000000; // размер буфера для записи в файл
+
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder();
+
+            ANTHILL_RecreateFile(fileLog); // пересоздать/очистить журнал
+
+            // Проверка параметров
+            //
+            if (args.Length == 1)
+            {
+                if (Int32.TryParse(args[0], out cntAnts))
+                {
+                    if (cntAnts < 1000 || cntAnts > 1000000000)
+                    {
+                        ANTHILL_WriteLog(fileLog, String.Format("Значение [{0}] не корректно. Должно быть от 1000 до 1000000000", cntAnts));
+                        return;
+                    }
+                }
+                else
+                {
+                    ANTHILL_WriteLog(fileLog, String.Format("Ошибка в параметре: {0}", args[0]));
+                    return;
+                }
+            }
+            if (args.Length > 1)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в количестве параметров: {0}", args.Length));
+                return;
+            }
+
+            // Старт
+            //
+            ANTHILL_WriteLog(fileLog, "===========================");
+            ANTHILL_WriteLog(fileLog, "== Начало подготовки тестовых данных");
+            ANTHILL_WriteLog(fileLog, "===========================");
+            ANTHILL_WriteLog(fileLog, String.Format("Задано: {0}", cntAnts));
+
+            try
+            {
+                // Создать anthill-ant-type.csv
+                //
+                ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntType));
+
+                ANTHILL_RecreateFile(fileAntType); // пересоздать/очистить файл
+
+                sb.Clear();
+                sb.Append("1\tЦарица").Append(Environment.NewLine);
+                sb.Append("2\tЛичинка").Append(Environment.NewLine);
+                sb.Append("3\tНянька").Append(Environment.NewLine);
+                sb.Append("4\tРабочий").Append(Environment.NewLine);
+                sb.Append("5\tСолдат").Append(Environment.NewLine);
+
+                ANTHILL_WriteFile(fileAntType, sb.ToString());
+
+                // Создать anthill-hill-type.csv
+                //
+                ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileHillType));
+
+                ANTHILL_RecreateFile(fileHillType); // пересоздать/очистить файл
+
+                sb.Clear();
+                sb.Append("1\tЦентр").Append(Environment.NewLine);
+                sb.Append("2\tВерх").Append(Environment.NewLine);
+                sb.Append("3\tНиз").Append(Environment.NewLine);
+                sb.Append("4\tСевер").Append(Environment.NewLine);
+                sb.Append("5\tЮг").Append(Environment.NewLine);
+                sb.Append("6\tВосток").Append(Environment.NewLine);
+                sb.Append("7\tЗапад").Append(Environment.NewLine);
+
+                ANTHILL_WriteFile(fileHillType, sb.ToString());
+
+                // Создать anthill-ant-list.csv
+                //
+                ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntList));
+
+                ANTHILL_RecreateFile(fileAntList); // пересоздать/очистить файл
+
+                sb.Clear();
+                sb.Append("1\t1").Append(Environment.NewLine); // царица - одна на весь муравейник
+                for (int n = 2; n <= cntAnts; n++)
+                {
+                    sb.Append(n.ToString()).Append("\t").Append(random.Next(2, 6).ToString()).Append(Environment.NewLine);
+
+                    // записать в файл
+                    if (n % cntBuff == 0)
+                    {
+                        ANTHILL_WriteFile(fileAntList, sb.ToString());
+                        sb.Clear();
+                        ANTHILL_WriteLog(fileLog, String.Format("Создано: {0}", n));
+                    }
+                }
+
+                // дописать в файл
+                if (sb.Length > 0)
+                    ANTHILL_WriteFile(fileAntList, sb.ToString());
+
+                // Создать anthill-cell-list.csv
+                //
+                ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileCellList));
+
+                ANTHILL_RecreateFile(fileCellList); // пересоздать/очистить файл
+
+                sb.Clear();
+                sb.Append("1\t1").Append(Environment.NewLine);
+                for (int n = 2; n <= cntAnts; n++)
+                {
+                    sb.Append(n.ToString()).Append("\t").Append(random.Next(1, 8).ToString()).Append(Environment.NewLine);
+
+                    // записать в файл
+                    if (n % cntBuff == 0)
+                    {
+                        ANTHILL_WriteFile(fileCellList, sb.ToString());
+                        sb.Clear();
+                        ANTHILL_WriteLog(fileLog, String.Format("Создано: {0}", n));
+                    }
+                }
+
+                // дописать в файл
+                if (sb.Length > 0)
+                    ANTHILL_WriteFile(fileCellList, sb.ToString());
+
+                // Создать anthill-ant-to-cell.csv
+                //
+                ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntToCell));
+
+                ANTHILL_RecreateFile(fileAntToCell); // пересоздать/очистить файл
+
+                sb.Clear();
+                for (int n = 1, c = cntAnts; n <= cntAnts; n++, c--)
+                {
+                    sb.Append(n.ToString()).Append("\t").Append(c.ToString()).Append(Environment.NewLine);
+
+                    // записать в файл
+                    if (n % cntBuff == 0)
+                    {
+                        ANTHILL_WriteFile(fileAntToCell, sb.ToString());
+                        sb.Clear();
+                        ANTHILL_WriteLog(fileLog, String.Format("Создано: {0}", n));
+                    }
+                }
+
+                // дописать в файл
+                if (sb.Length > 0)
+                    ANTHILL_WriteFile(fileAntToCell, sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка: [{0}]", ex.Message));
+                return;
+            }
+
+            // Завершение
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Итого: {0}", cntAnts));
+            ANTHILL_WriteLog(fileLog, "===========================");
+            ANTHILL_WriteLog(fileLog, "== Подготовка тестовых данных завершена");
+            ANTHILL_WriteLog(fileLog, "===========================");
+        }
+        //========
+        // Переоткрыть файл
+        //========
+        private static void ANTHILL_RecreateFile(string fileFullName)
+        {
+            if (File.Exists(fileFullName))
+                File.Delete(fileFullName);
+
+            using(File.Create(fileFullName));
+        }
+        //========
+        // Записать в файл
+        //========
+        private static void ANTHILL_WriteFile(string fileFullName, string strX)
+        {
+            strX = strX.TrimEnd(new char[] { '\n', '\r' }); 
+
+            if (File.Exists(fileFullName))
+                using (StreamWriter sw = File.AppendText(fileFullName))
+                    sw.WriteLine(strX);
+            else
+                using (StreamWriter sw = File.CreateText(fileFullName))
+                    sw.WriteLine(strX);
+        }
+        //========
+        // Записать в журнал
+        //========
+        private static void ANTHILL_WriteLog(string fileLog, string strLog)
+        {
+            strLog = "[" + DateTime.Now.ToString(@"hh\:mm\:ss") + "] " + strLog;
+            Console.WriteLine(strLog);
+            ANTHILL_WriteFile(fileLog, strLog);
+        }
+    }
+}
---
+++ anthill-makeresult.cs
@@ -0,0 +1,317 @@
+﻿// ============================
+// Подготовка файлов результата (для публикации "Учёт в муравейнике")
+// Для получения рабочей программы в Visual Studio надо создать консольный проект, заменить файл program.cs на anthill-makeresult.cs и собрать проект
+// Примечание: в свойствах проекта, в разделе BUILD, снять галочку [Prefer 32-bit]
+// Запускать программу следует в той-же директории, где находятся файлы тестовых данных
+//   anthill-ant-list.csv (список муравьёв)
+//   anthill-ant-type.csv (типы муравьёв)
+//   anthill-cell-list.csv (список домов)
+//   anthill-hill-type.csv (части (районы) муравейника)
+//   anthill-ant-to-cell.csv (расселение муравёв по ячейкам)
+// ============================
+// Разработчик: apelserg ; https://github.com/apelserg/
+// Лицензия: WTFPL
+// ============================
+
+using System;
+using System.Text;
+using System.IO;
+
+namespace anthill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileAntList = "anthill-ant-list.csv"; // список муравьёв
+            string fileAntType = "anthill-ant-type.csv"; // типы муравьёв
+            string fileCellList = "anthill-cell-list.csv"; // список домов
+            string fileHillType = "anthill-hill-type.csv"; // части (районы) муравейника
+            string fileAntToCell = "anthill-ant-to-cell.csv"; // расселение муравёв по ячейкам
+            string fileLog = "anthill-makeresult.log"; // журнал
+
+            int cntAnts = 0; // количество муравьёв (от 1000 до 1000000000)
+            int cntCells = 0; // количество домов (по количеству муравьёв - от 1000 до 1000000000)
+            int cntAntToCells = 0; // по количеству муравьёв (от 1000 до 1000000000)
+            int cntAntTypes = 0; // количество типов муравьёв (до 10, хотя в текущей реализации 5)
+            int cntHillTypes = 0; // количество частей муравейника (до 10, хотя в текущей реализации 7)
+            int cntBuff = 10000; // размер буфера для записи в файл (уменьшен до 10000, так как одновременно пишется несколько файлов)
+            int cntLog = 1000000; // через сколько циклов показывать инфо в консоли
+            int totalMax = 1000000000; // максимальное обрабатываемое количество (при нехватке памяти может быть уменьшено - соответственно должно быть уменьшено количество входных данных)
+            int filtrAntType = 5; // дефолтный фильтр - солдат
+
+            // 10 + 1 (по количеству частей муравейника - до 10 (в текущей реализации 7)
+            // единица добавлена для естественной нумерации: номер строки = номеру индекса
+            //
+            int[] sbCntBuff = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            StringBuilder[] sb = { /* 0 */ new StringBuilder(), /* 1 */ new StringBuilder(), /* 2 */ new StringBuilder(), /* 3 */ new StringBuilder(), /* 4 */ new StringBuilder(), /* 5 */ new StringBuilder(), /* 6 */ new StringBuilder(), /* 7 */ new StringBuilder(), /* 8 */ new StringBuilder(), /* 9 */ new StringBuilder(), /* 10 */ new StringBuilder() };
+
+            string[] antType = new string[10 + 1]; // массив типов муравьёв
+            string[] hillType = new string[10 + 1]; // массив типов частей муравейника
+            byte[] antList; // массив муравьёв
+            byte[] cellList; // массив ячеек
+
+            // пересоздать/очистить журнал
+            //
+            ANTHILL_RecreateFile(fileLog); 
+
+            // Выделение памяти
+            //
+            try
+            {
+                antList = new byte[totalMax + 1]; // массив муравьёв
+                cellList = new byte[totalMax + 1]; // массив домов
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка выделения памяти [{0}]", ex.Message));
+                return;
+            }
+
+            // Проверка параметров
+            //
+            if (args.Length == 1)
+            {
+                if (Int32.TryParse(args[0], out filtrAntType))
+                {
+                    if (filtrAntType < 2 || filtrAntType > 5)
+                    {
+                        ANTHILL_WriteLog(fileLog, String.Format("Значение [{0}] не корректно. Должно быть от 2 до 5", filtrAntType));
+                        return;
+                    }
+                }
+                else
+                {
+                    ANTHILL_WriteLog(fileLog, String.Format("Ошибка в параметре: {0}", args[0]));
+                    return;
+                }
+            }
+            if (args.Length > 1)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в количестве параметров: {0}", args.Length));
+                return;
+            }
+
+            // Старт
+            //
+            ANTHILL_WriteLog(fileLog, "===========================");
+            ANTHILL_WriteLog(fileLog, "== Начало обработки тестовых данных (формирование файлов результата)");
+            ANTHILL_WriteLog(fileLog, "===========================");
+
+            // считать файл anthill-ant-type.csv в массив
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntType));
+
+            try
+            {
+                using (StreamReader sReader = File.OpenText(fileAntType))
+                {
+                    while (!sReader.EndOfStream)
+                    {
+                        cntAntTypes++;
+                        string[] strList = sReader.ReadLine().Split(new char[] { '\t' });
+                        int idx = int.Parse(strList[0]);
+                        antType[idx] = strList[1];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в файле: {0}, строка {1} [{2}]", fileAntType, cntAntTypes, ex.Message));
+                return;
+            }
+
+            // считать файл anthill-hill-type.csv в массив
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileHillType));
+            
+            try
+            {
+                using (StreamReader sReader = File.OpenText(fileHillType))
+                {
+                    while (!sReader.EndOfStream)
+                    {
+                        cntHillTypes++;
+                        string[] strList = sReader.ReadLine().Split(new char[] { '\t' });
+                        int idx = int.Parse(strList[0]);
+                        hillType[idx] = strList[1];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в файле: {0}, строка {1} [{2}]", fileHillType, cntHillTypes, ex.Message));
+                return;
+            }
+
+            // считать файл anthill-ant-list.csv в массив
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntList));
+
+            try
+            {
+                using (StreamReader sReader = File.OpenText(fileAntList))
+                {
+                    while (!sReader.EndOfStream)
+                    {
+                        cntAnts++;
+                        string[] strList = sReader.ReadLine().Split(new char[] { '\t' });
+                        int idx = int.Parse(strList[0]);
+                        antList[idx] = byte.Parse(strList[1]);
+
+                        if (cntAnts % cntLog == 0)
+                            ANTHILL_WriteLog(fileLog, String.Format("Считано: {0}", cntAnts));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в файле: {0}, строка {1} [{2}]", fileAntList, cntAnts, ex.Message));
+                return;
+            }
+
+            // считать файл anthill-cell-list.csv в массив
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileCellList));
+
+            try
+            {
+                using (StreamReader sReader = File.OpenText(fileCellList))
+                {
+                    while (!sReader.EndOfStream)
+                    {
+                        cntCells++;
+                        string[] strList = sReader.ReadLine().Split(new char[] { '\t' });
+                        int idx = int.Parse(strList[0]);
+                        cellList[idx] = byte.Parse(strList[1]);
+
+                        if (cntCells % cntLog == 0)
+                            ANTHILL_WriteLog(fileLog, String.Format("Считано: {0}", cntCells));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в файле: {0}, строка {1} [{2}]", fileCellList, cntCells, ex.Message));
+                return;
+            }
+
+            // считать файл anthill-ant-to-cell.csv и создать файлы результата
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Файл: {0}", fileAntToCell));
+
+            try
+            {
+                // очистить/пересоздать файлы результата
+                //
+                foreach (string s in hillType)
+                    if (!String.IsNullOrEmpty(s))
+                        ANTHILL_RecreateFile(ANTHILL_GetFileName(s, antType[filtrAntType]));
+
+                int currAnt, currCell;
+                int currAntType, currHillType;
+                string currAntTypeName, currHillTypeName;
+
+                using (StreamReader sReader = File.OpenText(fileAntToCell))
+                {
+                    while (!sReader.EndOfStream)
+                    {
+                        // считать строку
+                        //
+                        cntAntToCells++;
+                        string[] strList = sReader.ReadLine().Split(new char[] { '\t' });
+
+                        currAnt = int.Parse(strList[0]);
+                        currCell = int.Parse(strList[1]);
+                        currAntType = antList[currAnt];
+                        currHillType = cellList[currCell];
+                        currAntTypeName = antType[currAntType];
+                        currHillTypeName = hillType[currHillType];
+
+                        if (cntAntToCells % cntLog == 0)
+                            ANTHILL_WriteLog(fileLog, String.Format("Считано: {0}", cntAntToCells));
+
+                        // записать результат
+                        //
+                        if (currAntType == filtrAntType)
+                        {
+                            sbCntBuff[currHillType]++;
+                            sb[currHillType].Append(currAnt.ToString()).Append("\t").Append(currCell.ToString()).Append(Environment.NewLine);
+
+                            if (sbCntBuff[currHillType] % cntBuff == 0)
+                            {
+                                string strFileName = ANTHILL_GetFileName(currHillTypeName, antType[filtrAntType]);
+
+                                ANTHILL_WriteFile(strFileName, sb[currHillType].ToString());
+                                sb[currHillType].Clear();
+
+                                ANTHILL_WriteLog(fileLog, String.Format("Запись в файл: {0}, строк {1}", strFileName, sbCntBuff[currHillType]));
+                            }
+                        }
+
+                    }
+
+                    // дописать файлы
+                    //
+                    for (int n = 1; n < hillType.Length; n++)
+                        if (!String.IsNullOrEmpty(hillType[n]))
+                            if (sb[n].Length > 0)
+                                ANTHILL_WriteFile(ANTHILL_GetFileName(hillType[n], antType[filtrAntType]), sb[n].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                ANTHILL_WriteLog(fileLog, String.Format("Ошибка в файле: {0}, строка {1} [{2}]", fileAntToCell, cntAntToCells, ex.Message));
+                return;
+            }
+
+            // Завершение
+            //
+            ANTHILL_WriteLog(fileLog, String.Format("Обработано: {0}", cntAnts));
+            ANTHILL_WriteLog(fileLog, "===========================");
+            ANTHILL_WriteLog(fileLog, "== Тестовые данные обработаны (файлы с результатами готовы)");
+            ANTHILL_WriteLog(fileLog, "===========================");
+
+        }
+        //========
+        // Переоткрыть файл
+        //========
+        private static void ANTHILL_RecreateFile(string fileFullName)
+        {
+            if (File.Exists(fileFullName))
+                File.Delete(fileFullName);
+
+            using(File.Create(fileFullName));
+        }
+        //========
+        // Записать в файл
+        //========
+        private static void ANTHILL_WriteFile(string fileFullName, string strX)
+        {
+            strX = strX.TrimEnd(new char[] { '\n', '\r' }); 
+
+            if (File.Exists(fileFullName))
+                using (StreamWriter sw = File.AppendText(fileFullName))
+                    sw.WriteLine(strX);
+            else
+                using (StreamWriter sw = File.CreateText(fileFullName))
+                    sw.WriteLine(strX);
+        }
+        //========
+        // Записать в журнал
+        //========
+        private static void ANTHILL_WriteLog(string fileLog, string strLog)
+        {
+            strLog = "[" + DateTime.Now.ToString(@"hh\:mm\:ss") + "] " + strLog;
+            Console.WriteLine(strLog);
+            ANTHILL_WriteFile(fileLog, strLog);
+        }
+        //========
+        // Новое имя файла
+        //========
+        private static string ANTHILL_GetFileName(string strHillType, string strAntType)
+        {
+            return "anthill-" + strHillType + "-" + strAntType + ".csv";
+        }
+    }
+}
